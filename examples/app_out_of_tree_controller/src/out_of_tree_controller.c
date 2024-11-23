71d5a95caa034d1bdc2802e33a6e11ed667cb0a3
Jonathan Lane <jglane@purdue.edu>
2024-11-23 14:40:44 -0500

    Implemented true Taeyoung Lee controller in out_of_tree_controller.c

--- examples/app_out_of_tree_controller/src/out_of_tree_controller.c
+++ examples/app_out_of_tree_controller/src/out_of_tree_controller.c
@@ -54,23 +54,133 @@ void appMain() {
 
 // Call the PID controller in this example to make it possible to fly. When you implement you own controller, there is
 // no need to include the pid controller.
-#include "controller_pid.h"
+#include "math3d.h"
+#include "controller_lee.h"
+#include "platform_defaults.h"
+#include "physicalConstants.h"
+
+static inline struct mat33 mlog(struct mat33 R) {
+	float acosinput = (R.m[0][0] + R.m[1][1] + R.m[2][2] - 1) / 2.0f;
+	if (acosinput >= 1) {
+		return mzero();
+	} else if (acosinput <= -1) {
+		struct vec omg;
+		if (!(fabsf(1.0f + R.m[2][2]) < 1e-6f)) {
+			omg = vdiv(mkvec(R.m[0][2], R.m[1][2], 1.0f + R.m[2][2]), sqrtf(2.0f * (1.0f + R.m[2][2])));
+		} else if (!(fabsf(1.0f + R.m[1][1]) < 1e-6f)) {
+			omg = vdiv(mkvec(R.m[0][1], 1.0f + R.m[1][1], R.m[2][1]), sqrtf(2.0f * (1.0f + R.m[1][1])));
+		} else {
+			omg = vdiv(mkvec(1.0f + R.m[0][0], R.m[1][0], R.m[2][0]), sqrtf(2.0f * (1.0f + R.m[0][0])));
+		}
+		return mcrossmat(vscl(M_PI_F, omg));
+	} else {
+		float theta = acosf(acosinput);
+		return mscl(theta / 2.0f / sinf(theta), msub(R, mtranspose(R)));
+	}
+}
+
+static inline struct vec mvee(struct mat33 R) {
+	return mkvec(R.m[2][1], R.m[0][2], R.m[1][0]);
+}
+
+static controllerLee_t g_self2 = {
+  .mass = CF_MASS,
+
+  // Inertia matrix (diagonal matrix), see
+  // System Identification of the Crazyflie 2.0 Nano Quadrocopter
+  // BA theses, Julian Foerster, ETHZ
+  // https://polybox.ethz.ch/index.php/s/20dde63ee00ffe7085964393a55a91c7
+  .J = {16.571710e-6, 16.655602e-6, 29.261652e-6}, // kg m^2
+
+  // Position PID
+  .Kpos_P = {7.0, 7.0, 7.0}, // Kp in paper
+  .Kpos_P_limit = 100,
+  .Kpos_D = {4.0, 4.0, 4.0}, // Kv in paper
+  .Kpos_D_limit = 100,
+  .Kpos_I = {0.0, 0.0, 0.0}, // not in paper
+  .Kpos_I_limit = 2,
+
+  // Attitude PID
+  .KR = {0.007, 0.007, 0.008},
+  .Komega = {0.00115, 0.00115, 0.002},
+  .KI = {0.03, 0.03, 0.03},
+
+  // New stuff
+  .R_des.m = {
+    {1.0f, 0.0f, 0.0f},
+    {0.0f, 1.0f, 0.0f},
+    {0.0f, 0.0f, 1.0f}
+  },
+  .omega_r = {0.0f, 0.0f, 0.0f}
+};
 
 void controllerOutOfTreeInit() {
-  // Initialize your controller data here...
-
-  // Call the PID controller instead in this example to make it possible to fly
-  controllerPidInit();
+  controllerLeeInit(&g_self2);
 }
 
 bool controllerOutOfTreeTest() {
-  // Always return true
-  return true;
+  return controllerLeeFirmwareTest();
 }
 
 void controllerOutOfTree(control_t *control, const setpoint_t *setpoint, const sensorData_t *sensors, const state_t *state, const uint32_t tick) {
-  // Implement your controller here...
+  if (!RATE_DO_EXECUTE(ATTITUDE_RATE, tick)) {
+    return;
+  }
+  
+  controllerLee_t self = g_self2;
+  float dt = (float)(1.0f/ATTITUDE_RATE);
+  
+  struct vec x = mkvec(state->position.x, state->position.y, state->position.z);
+  struct vec v = mkvec(state->velocity.x, state->velocity.y, state->velocity.z);
+  struct mat33 R = quat2rotmat(mkquat(state->attitudeQuaternion.x, state->attitudeQuaternion.y, state->attitudeQuaternion.z, state->attitudeQuaternion.w));
+  struct vec W = mkvec(sensors->gyro.x, sensors->gyro.y, sensors->gyro.z);
 
-  // Call the PID controller instead in this example to make it possible to fly
-  controllerPid(control, setpoint, sensors, state, tick);
+  struct vec x_d = mkvec(setpoint->position.x, setpoint->position.y, setpoint->position.z);
+  struct vec v_d = mkvec(setpoint->velocity.x, setpoint->velocity.y, setpoint->velocity.z);
+  struct vec a_d = mkvec(setpoint->acceleration.x, setpoint->acceleration.y, setpoint->acceleration.z);
+  struct vec b1_d = mcolumn(maxisangle(vbasis(2), radians(setpoint->attitude.yaw)), 0);
+
+  struct vec ex = vsub(x, x_d);
+  struct vec ev = vsub(v, v_d);
+  
+  struct vec F_d = vadd4(
+    vneg(veltmul(self.Kpos_P, ex)),
+    vneg(veltmul(self.Kpos_D, ev)),
+    vscl(self.mass, a_d),
+    vscl(self.mass*GRAVITY_MAGNITUDE, vbasis(2)));
+  struct vec b3_d = vnormalize(F_d);
+  struct vec b2_d = vnormalize(vcross(b3_d, b1_d));
+  struct mat33 R_d = mcolumns(vcross(b2_d, b3_d), b2_d, b3_d);
+
+  float f = 0.0f;
+  struct vec M = vzero();
+
+  if (vneq(mcolumn(self.R_des, 0), vbasis(0)) && veq(mcolumn(self.R_des, 1), vbasis(1)) && veq(mcolumn(self.R_des, 2), vbasis(2))) {
+    struct vec W_d = mvee(mscl(1.0f/dt, mlog(mmul(mtranspose(self.R_des), R_d))));
+    if (vneq(self.omega_r, vzero())) {
+      struct vec W_d_dot = vdiv(vsub(W_d, self.omega_r), dt);
+
+      struct vec eR = vscl(0.5f, mvee(msub(
+        mmul(mtranspose(R_d), R),
+        mmul(mtranspose(R), R_d))));
+      struct vec eW = vsub(W, mvmul(mtranspose(R), mvmul(R_d, W_d)));
+
+      f = vdot(F_d, mvmul(R, vbasis(2)));
+      M = vadd4(
+        vneg(veltmul(self.KR, eR)),
+        vneg(veltmul(self.Komega, eW)),
+        vcross(W, veltmul(self.J, W)),
+        vneg(veltmul(self.J, vsub(
+          vcross(W, mvmul(mtranspose(R), mvmul(R_d, W_d))),
+          mvmul(mtranspose(R), mvmul(R_d, W_d_dot))))));
+    }
+    self.omega_r = W_d;
+  }  
+  self.R_des = R_d;
+
+  control->controlMode = controlModeForceTorque;
+  control->thrustSi = f;
+  control->torque[0] = M.x;
+  control->torque[1] = M.y;
+  control->torque[2] = M.z;
 }
