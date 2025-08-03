68891f4c03e82abaac65318a8158cc089b742a4d
Jonathan Lane <jglane@purdue.edu>
2025-08-03 01:44:59 -0400

    Added task to flash light

--- src/config/config.h
+++ src/config/config.h
@@ -101,6 +101,7 @@
 #define UART2_TASK_PRI            3
 #define CRTP_SRV_TASK_PRI         0
 #define PLATFORM_SRV_TASK_PRI     0
+#define MAGNETDECK_TASK_PRI       2
 
 // Not compiled
 #if 0
@@ -160,6 +161,7 @@
 #define CPX_TASK_NAME             "CPX"
 #define APP_TASK_NAME             "APP"
 #define FLAPPERDECK_TASK_NAME     "FLAPPERDECK"
+#define MAGNETDECK_TASK_NAME      "MAGNETDECK"
 
 
 //Task stack sizes
@@ -207,6 +209,7 @@
 #define KALMAN_TASK_STACKSIZE           (3 * configMINIMAL_STACK_SIZE)
 #define FLAPPERDECK_TASK_STACKSIZE      (2 * configMINIMAL_STACK_SIZE)
 #define ERROR_UKF_TASK_STACKSIZE        (4 * configMINIMAL_STACK_SIZE)
+#define MAGNETDECK_TASK_STACKSIZE       (2 * configMINIMAL_STACK_SIZE)
 
 //The radio channel. From 0 to 125
 #define RADIO_CHANNEL 80
--- src/deck/drivers/src/magnetdeck.c
+++ src/deck/drivers/src/magnetdeck.c
@@ -2,24 +2,42 @@
 #include "debug.h"
 
 #include "deck.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
+#include "system.h"
+#include "platform_defaults.h"
+
+#include "log.h"
+
+void magnetDeckTask();
 
 static void magnetInit() {
   DEBUG_PRINT("Hello Crazyflie 2.1 deck world!\n");
 
   pinMode(DECK_GPIO_IO1, OUTPUT);
+
+  xTaskCreate(magnetDeckTask, MAGNETDECK_TASK_NAME, MAGNETDECK_TASK_STACKSIZE, NULL, MAGNETDECK_TASK_PRI, NULL);
 }
 
 static bool magnetTest() {
-  digitalWrite(DECK_GPIO_IO1, HIGH);
-  vTaskDelay(M2T(1000));
-  digitalWrite(DECK_GPIO_IO1, LOW);
-
-  DEBUG_PRINT("Hello test passed!\n");
-
   return true;
+}
+
+void magnetDeckTask() {
+  systemWaitStart();
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  logVarId_t logIdControllerCmdThrust = logGetVarId("controller", "cmd_thrust");
+
+  while (1) {
+    vTaskDelayUntil(&xLastWakeTime, M2T(100)); // 10 Hz
+
+    if (logGetFloat(logIdControllerCmdThrust) > PID_VEL_THRUST_BASE) {
+      digitalWrite(DECK_GPIO_IO1, HIGH);
+    } else {
+      digitalWrite(DECK_GPIO_IO1, LOW);
+    }
+  }
 }
 
 static const DeckDriver magnetDriver = {
