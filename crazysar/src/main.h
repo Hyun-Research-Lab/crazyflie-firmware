f38fa7d742d5a335dadbf992ed70f03b989fc530
Jonathan Lane <jglane@purdue.edu>
2026-05-30 11:37:02 -0400

    Make rod length 40 cm

--- crazysar/src/main.h
+++ crazysar/src/main.h
@@ -5,7 +5,7 @@
 #define CRAZYSAR_NETWORK_RATE RATE_100_HZ
 #define CRAZYSAR_ATTITUDE_RATE RATE_500_HZ
 
-#define ROD_LENGTH 0.395f
+#define ROD_LENGTH 0.4f
 
 #define LED_LEADER   0b10110101 // red and blue
 #define LED_ROOT     LED_LEADER //0b10101011 // green and blue
